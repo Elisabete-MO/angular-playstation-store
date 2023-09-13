35284d9a27b7a1a4d54475f007dcc1a86a1ddacd
Bete.mrt <bete.mrt@gmail.com>
2023-09-13 16:32:15 -0300

    :lipstick: feat: header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
